beb07aba388bea5d9057a8f8f38b68218458b48e
root <root@portal.(none)>
2012-09-03 16:51:43 +0800

    Добавление сортировки в поля show и их ограничение

--- app/models/comp.rb
+++ app/models/comp.rb
@@ -1,5 +1,9 @@
 class Comp < ActiveRecord::Base
-  attr_accessible :name
+  attr_accessible :name , :sortuserlog
   has_many :userlog
   has_many :user, :through => :userlog
+  
+  def sortuserlog
+    self.userlog.find(:all, :order => "created_at DESC", :limit => 30)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :name
   has_many :userlog
   has_many :comp, :through => :userlog
+
+  def sortuserlog
+    self.userlog.find(:all, :order => "created_at DESC", :limit => "30")
+  end
 end
